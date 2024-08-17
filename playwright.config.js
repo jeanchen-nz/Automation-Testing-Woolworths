ac5eb2e00161dca871003044660388f95db3aae8
Jean Chen <jeanchen.nz@gmail.com>
2024-08-17 22:53:15 +1200

    valid registration

--- playwright.config.js
+++ playwright.config.js
@@ -13,7 +13,7 @@ const { defineConfig, devices } = require('@playwright/test');
 module.exports = defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
-  timeout: 30000,
+  timeout: 30*1000,
   expect:{
     timeout:5*1000
   },
@@ -30,7 +30,7 @@ module.exports = defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
-
+    headless: false,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
--- tests/registration/validRegistration.spec.js
+++ tests/registration/validRegistration.spec.js
@@ -1,11 +1,34 @@
-const {test,expect} = require('@playwright/test');
+const { test, expect } = require('@playwright/test');
 const exp = require('constants');
 
-test("Valid Registration", async({page})=>
-{
+test("Valid Registration", async ({ page }) => {
     await page.goto("https://www.woolworths.co.nz/");
-    await expect(page).toHaveTitle("Woolworths");
-
-
-
-})
+    await page.locator('button:has-text("Sign in or Register")').click();
+    await page.locator("text='Register for online shopping'").click();
+    await page.waitForLoadState('networkidle');
+    await page.locator("#emailInput").fill('wesseicahocru-9326@yopmail.com');
+    await page.getByTestId('submitButton').click();
+    await page.waitForLoadState('domcontentloaded');
+    await page.getByTestId('passwordInput').fill('Woolworths@2024');
+    await page.locator("button#showPassword").click();
+    await page.locator('.submit-button').click();
+    await page.getByLabel('First name *').fill('Mary');
+    await page.locator('#lastName').fill('Lee');
+    await page.locator('#dobDay').fill('11');
+    await page.locator('#dobMonth').fill('12');
+    await page.locator('#dobYear').fill('1999');
+    await page.locator('#mobile').fill('022 982 829');
+    await page.locator('label:has-text("Please send me personalised offers and updates from Woolworths.")').click();
+    await page.getByTestId('submit').click();
+    await page.waitForTimeout(7000);
+    await page.locator('.inputText').fill('PO Box 89084, Torbay, Aucklan');
+    await page.keyboard.press('d');
+    await page.waitForTimeout(1000);
+    await page.keyboard.press('ArrowDown');
+    await page.keyboard.press('Enter');
+    await page.getByRole('option', { name: 'PO Box 89084, Torbay, Auckland' }).click();
+    await page.waitForTimeout(1000);
+    await page.getByRole('button', { name: 'Add my delivery address' }).click();
+    await page.getByRole('link', { name: 'Continue shopping from the' }).click();
+    await expect(page.locator('text="Mary, welcome to Woolworths!"')).toBeVisible();
+});
