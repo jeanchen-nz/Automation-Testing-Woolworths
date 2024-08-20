636eb726757a154cc97ecfa2ca12998d51bb0efc
Jean Chen <jeanchen.nz@gmail.com>
2024-08-20 23:00:19 +1200

    Home phone update

--- tests/profile/mobileUpdate.spec.js
+++ tests/profile/mobileUpdate.spec.js
@@ -7,14 +7,25 @@ test("Mobile Update", async ({ page }) => {
     
     const loginPage = new LoginPage(page);
     await loginPage.login('testww@mail.com', 'Countdown@2024');
-    //const isLoggedIn = await loginPage.isLoggedIn();
-    //expect(isLoggedIn).toBeTruthy();
     await page.getByRole('button', { name: 'Kia ora, Mary' }).click();
     await page.getByRole('link', { name: 'More account options' }).click();
-    //await page.waitForLoadState('domcontentloaded');
     await page.getByRole('link', { name: 'Personal details' }).click();
     await page.locator('#mobilePhone').fill("");
     await page.locator('#mobilePhone').fill('027123112');
+    await page.getByRole('button', { name: 'Update details' }).click();
+    await page.getByRole('button', { name: 'Details updated' }).isVisible();
+
+
+});
+
+test("Home Phone Update", async ({ page }) => {
+    
+    const loginPage = new LoginPage(page);
+    await loginPage.login('testww@mail.com', 'Countdown@2024');
+    await page.getByRole('button', { name: 'Kia ora, Mary' }).click();
+    await page.getByRole('link', { name: 'More account options' }).click();
+    await page.getByRole('link', { name: 'Personal details' }).click();
+    await page.locator('#homePhone').fill("095123123");
     await page.getByRole('button', { name: 'Update details' }).click();
     await page.getByRole('button', { name: 'Details updated' }).isVisible();
 
