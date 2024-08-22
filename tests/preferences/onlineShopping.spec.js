c2a1f10a90f9f35702bf4f3a8c26baebb014fec2
Jean Chen <jeanchen.nz@gmail.com>
2024-08-23 11:46:19 +1200

    Other preferences

--- tests/preferences/onlineShopping.spec.js
+++ tests/preferences/onlineShopping.spec.js
@@ -7,16 +7,29 @@ test("packing slip", async ({ page }) => {
     
     const loginPage = new LoginPage(page);
     await loginPage.login('testww@mail.com', 'Countdown@2024');
-   
     await page.getByRole('button', { name: 'Kia ora, Mary' }).click();
     await page.getByRole('link', { name: 'More account options' }).click();
     await page.getByRole('link', { name: 'Online shopping preferences' }).click();
     await page.locator('#digital-packing-slip').click();
     await expect(page.locator('#digital-packing-slip')).toBeChecked();
     await page.getByRole('button', { name: 'Update preferences' }).click();
-    await page.pause(); 
     await page.locator('text="Preferences updated"').isVisible();
     
+
+});
+
+test("other preferences", async ({ page }) => {
+    
+    const loginPage = new LoginPage(page);
+    await loginPage.login('testww@mail.com', 'Countdown@2024');
+    await page.getByRole('button', { name: 'Kia ora, Mary' }).click();
+    await page.getByRole('link', { name: 'More account options' }).click();
+    await page.getByRole('link', { name: 'Online shopping preferences' }).click();
+    await page.getByText('Yes, I\'d like to receive').click();
+    await page.getByText('Show \“Have you forgotten').click();
+    await page.getByText('Default \“Yes').click();
+    await page.getByRole('button', { name: 'Update preferences' }).click();
+    await page.locator('text="Preferences updated"').isVisible();
 
 
 });
