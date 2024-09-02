83958ecf4b276f860e77a869bd61eec2c54b3b73
Jean Chen <jeanchen.nz@gmail.com>
2024-09-02 14:54:25 +1200

    Auto suggestion for search bar

--- tests/search/searchBar.spec.js
+++ tests/search/searchBar.spec.js
@@ -35,5 +35,29 @@ test('searching for popular items', async ({ page }) => {
         const itemFound = texts.includes(item);
         expect(itemFound).toBeTruthy();
     }
-
 });
+
+test('Auto suggestion for search bar', async ({ page }) => {
+   
+    const searchBar = page.locator('#search'); 
+
+    await searchBar.click();
+    await page.keyboard.press('M');
+    await page.keyboard.press('I');
+
+
+    const suggestions = await page.getByRole('option').all();
+    
+    let texts = [];
+    for (const suggestion of suggestions) {
+        const text = await suggestion.innerText();
+        texts.push(text);
+        console.log(text);
+    }
+
+    for (const text of texts) {
+        expect(text.toLowerCase()).toContain('mi');
+    }
+});
+
+
