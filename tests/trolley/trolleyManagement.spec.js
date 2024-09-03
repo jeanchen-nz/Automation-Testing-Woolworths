131b83fb5af590657d82b924118f0494fc9b0899
Jean Chen <jeanchen.nz@gmail.com>
2024-09-03 22:29:39 +1200

    slight changes

--- tests/trolley/trolleyManagement.spec.js
+++ tests/trolley/trolleyManagement.spec.js
@@ -6,22 +6,17 @@ test.beforeEach(async ({ page }) => {
     await page.goto('https://www.woolworths.co.nz/');
 });
 
-test('adding items', async ({ page }) => {
+test('adding item to trolley', async ({ page }) => {
 
     const searchBar = page.locator('#search');
 
     await searchBar.click();
     await page.keyboard.type('Milk');
     await page.keyboard.press('Enter');
-    await page.pause();
-    //await page.waitForLoadState('networkidle');
-    //locator('cdx-card').first()
-    await page.locator('.addToTrolley-btn--add').first().click();
-    //await page.waitForLoadState('networkidle');
 
+    await page.locator('.addToTrolley-btn--add').first().click();
 
     await page.getByRole('link', { name: 'Review order or checkout $' }).click();
-    //await page.waitForLoadState('networkidle');
 
     const item = page.locator('#product-282843-title');
     const title = await item.innerText();
