0977a15b1876774f2885e93be678ef426452d069
Jean Chen <jeanchen.nz@gmail.com>
2024-09-14 16:39:18 +1200

    View saved list

--- tests/shoppingList/savedList.spec.js
+++ tests/shoppingList/savedList.spec.js
@@ -24,3 +24,19 @@ test('create saved list', async ({ page }) => {
 
 
 });
+
+test('view saved list', async ({ page }) => {
+    
+        await page.getByText('Favourites & lists').click();
+        await page.getByRole('link', { name: 'Saved lists' }).click();
+        await page.waitForLoadState('networkidle');
+
+        // const header = await page.getByRole('heading', { name: 'My saved lists' }).innerText();
+        // expect header.toBe("My saved lists");
+    
+        const list = page.getByRole('link', { name: 'Milk' });
+        const count = await list.count();
+        expect(count).toBe(1);
+    
+
+    });
