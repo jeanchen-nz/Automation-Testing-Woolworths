84d14ac5b5b07053c5f04d742d6c5b378d29c436
Jean Chen <jeanchen.nz@gmail.com>
2024-10-03 14:49:56 +1300

    view and delete personal recipes

--- tests/Recipes/personalRecipes.spec.js
+++ tests/Recipes/personalRecipes.spec.js
@@ -23,3 +23,26 @@ test('create personal recipes', async ({ page}) => {
 
 
 });
+
+test('view personal recipes', async ({ page }) => {
+    await page.locator('span:has-text("Recipes")').click();
+    await page.locator('text="Saved Recipes"').click();
+    const recipesTitle = page.getByLabel('Prep & Cook Time5 mins + 10')
+    await expect(recipesTitle).toBeVisible();
+
+});
+
+test('delete personal recipes', async ({ page }) => {
+    await page.locator('span:has-text("Recipes")').click();
+    await page.locator('text="Saved Recipes"').click();
+    await page.locator('.recipeStamp-image').click();
+    
+    await page.getByRole('button', { name: 'Saved', exact: true }).click();
+
+    await page.locator('span:has-text("Recipes")').click();
+    await page.locator('text="Saved Recipes"').click();
+
+    expect(page.locator('.listContent-errorMessage')).toBeVisible();
+
+
+});
